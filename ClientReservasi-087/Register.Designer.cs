1f07bea1cfc90d99ac8997bda3974582ff354144
DESKTOP-ND6HMKD\Fanky <andryfanky3@gmail.com>
2021-12-23 22:59:46 +0700

    Add form register and login

---
+++ ClientReservasi-087/Register.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace ClientReservasi_087
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.cmbKategori = new System.Windows.Forms.ComboBox();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dgRegister = new System.Windows.Forms.DataGridView();
+            this.tbID = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgRegister)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kategori";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(171, 44);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(349, 20);
+            this.tbUsername.TabIndex = 3;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(171, 100);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(349, 20);
+            this.tbPassword.TabIndex = 4;
+            // 
+            // cmbKategori
+            // 
+            this.cmbKategori.FormattingEnabled = true;
+            this.cmbKategori.Location = new System.Drawing.Point(171, 154);
+            this.cmbKategori.Name = "cmbKategori";
+            this.cmbKategori.Size = new System.Drawing.Size(349, 21);
+            this.cmbKategori.TabIndex = 5;
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(592, 37);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(75, 23);
+            this.btnSimpan.TabIndex = 6;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(592, 83);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(592, 129);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 23);
+            this.btnHapus.TabIndex = 8;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(592, 173);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // dgRegister
+            // 
+            this.dgRegister.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgRegister.Location = new System.Drawing.Point(29, 215);
+            this.dgRegister.Name = "dgRegister";
+            this.dgRegister.Size = new System.Drawing.Size(660, 150);
+            this.dgRegister.TabIndex = 10;
+            this.dgRegister.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgRegister_CellClick);
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(171, 12);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(122, 20);
+            this.tbID.TabIndex = 11;
+            this.tbID.Visible = false;
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 387);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.dgRegister);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.cmbKategori);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Register";
+            this.Text = "Register";
+            this.Load += new System.EventHandler(this.Register_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgRegister)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.ComboBox cmbKategori;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView dgRegister;
+        private System.Windows.Forms.TextBox tbID;
+    }
+}
---
+++ ClientReservasi-087/Register.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientReservasi_087
+{
+    public partial class Register : Form
+    {
+        ServiceReference1.Service1Client service = new ServiceReference1.Service1Client();
+        public Register()
+        {
+            InitializeComponent();
+            TampilData();
+            tbID.Visible = false;
+            btnUpdate.Enabled = false;
+            btnHapus.Enabled = false;
+        }
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            string username = tbUsername.Text;
+            string password = tbPassword.Text;
+            string kategori = cmbKategori.Text;
+            string a = service.Register(username, password, kategori);
+
+            if (tbUsername.Text == "" || tbPassword.Text == "" || cmbKategori.Text == "")
+            {
+                MessageBox.Show("Semua data wajib diisi.");
+            } else
+            {
+                MessageBox.Show(a);
+                Refresh();
+                TampilData();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string username = tbUsername.Text;
+            string password = tbPassword.Text;
+            string kategori = cmbKategori.Text;
+            int id = Convert.ToInt32(tbID.Text);
+            string a = service.UpdateRegister(username, password, kategori, id);
+
+            if (tbUsername.Text == "" || tbPassword.Text == "" || cmbKategori.Text == "")
+            {
+                MessageBox.Show("Semua data wajib diisi.");
+            } else
+            {
+                MessageBox.Show(a);
+                Clear();
+                TampilData();
+            }
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            string username = tbUsername.Text;
+
+            DialogResult dialogResult = MessageBox.Show("Apakah anda yakin ingin menghapus data ini?", "Hapus Data", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                string a = service.DeleteRegister(username);
+                MessageBox.Show(a);
+                Clear();
+                TampilData();
+            } else if (dialogResult == DialogResult.No)
+            {
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        public void TampilData()
+        {
+            var list = service.DataRegist();
+            dgRegister.DataSource = list;
+        }
+
+        public void Clear()
+        {
+            tbUsername.Clear();
+            tbPassword.Clear();
+            cmbKategori.SelectedItem = null;
+
+            btnSimpan.Enabled = true;
+            btnUpdate.Enabled = false;
+            btnHapus.Enabled = false;
+        }
+
+        private void dgRegister_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tbID.Text = Convert.ToString(dgRegister.Rows[e.RowIndex].Cells[0].Value);
+            tbUsername.Text = Convert.ToString(dgRegister.Rows[e.RowIndex].Cells[1].Value);
+            tbPassword.Text = Convert.ToString(dgRegister.Rows[e.RowIndex].Cells[2].Value);
+            cmbKategori.Text = Convert.ToString(dgRegister.Rows[e.RowIndex].Cells[3].Value);
+
+            btnUpdate.Enabled = true;
+            btnHapus.Enabled = true;
+
+            btnSimpan.Enabled = false;
+        }
+    }
+}
